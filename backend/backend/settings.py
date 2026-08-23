360602f7b7fbbe72941c27bd4cd0e4e029181d44
Pragyaa Rathore <pragyaa-19@users.noreply.github.com>
2026-08-23 16:20:26 +0530

    Add CSRF trusted origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,6 +86,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://smart-expense-manager-web.vercel.app",
+] 
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
